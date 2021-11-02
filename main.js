7aed00d4db0a5e4b0bd244432432d4f3b541cd7f
Robert Oliveira <robertsudoliveira@gmail.com>
2021-11-01 23:58:40 -0300

    adicionado parte de services

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle')
+/*abre e fecha o menu*/
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
 
-for (const element of toggle){
-  element.addEventListener('click' , function (){
-    nav.classList.toggle('show')
-  })
+for (const element of toggle) {
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
+}
+/*quando clicar em um item do menu, esconder  o menu*/
+
+const links = document.querySelectorAll("nav ul li a");
+
+for (const link of links) {
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
 }
