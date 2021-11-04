1c70c59de0993c214199ee783b126996917f3a34
Robert Oliveira <robertsudoliveira@gmail.com>
2021-11-03 23:39:34 -0300

    Inicializado o footer

--- main.js
+++ main.js
@@ -30,3 +30,15 @@ window.addEventListener('scroll', function(){
     header.classList.remove('scroll')
   }
 })
+
+
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination:{
+    el:'.swiper-pagination'
+  },
+  mousewheel:true,
+  keyboard:true,
+
+});
